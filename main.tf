6add99d8207770e4c5d56a29aec44ba1bc47aba2
mattj <matt.jones@rightmove.co.uk>
2018-09-18 20:48:52 +0100

    Stage 2. Simple Static S3 Website

--- main.tf
+++ main.tf
@@ -4,4 +4,30 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform_intro_demo_bucket" {
   bucket = "terraform-intro-demo-bucket"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Sid": "PublicReadForGetBucketObjects",
+    "Effect": "Allow",
+    "Principal": "*",
+    "Action": "s3:GetObject",
+    "Resource": ["arn:aws:s3:::terraform-intro-demo-bucket/*"]
+  }]
+}
+EOF
+}
+
+resource "aws_s3_bucket_object" "river_island_tech_demo_index_html" {
+  key          = "index.html"
+  bucket       = "${aws_s3_bucket.terraform_intro_demo_bucket.bucket}"
+  source       = "index.html"
+  content_type = "text/html"
+  etag         = "${md5(file("index.html"))}"
 }
